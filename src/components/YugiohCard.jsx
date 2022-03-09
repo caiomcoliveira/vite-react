a66242d2ffd5b4435d80deaad7a3fb6d51dc58ee
Caio Oliveira <caio.oliveira@redes.unb.br>
2022-03-09 08:50:02 -0300

    Card example

--- src/components/YugiohCard.jsx
+++ src/components/YugiohCard.jsx
@@ -1,6 +1,9 @@
 function YugiohCard() {
-    return <>CARD VIEW</>;
-  }
-  
-  export default YugiohCard;
-  
+  return (
+    <div className="yugioh-card-view-wrapper">
+      <div className="yugioh-card-view">CARD</div>
+    </div>
+  );
+}
+
+export default YugiohCard;
--- src/components/YugiohCardForm.jsx
+++ src/components/YugiohCardForm.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
+
 function YugiohCardForm() {
-  return <>CARD FORM</>;
+
+  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(email, password);
+  }
+
+
+  return (
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit} noValidate>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Card Name</label>
+            <input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default YugiohCardForm;
