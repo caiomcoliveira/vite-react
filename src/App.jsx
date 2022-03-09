ff9d4a4cb699b1e299a833ca9c13fbdbc2895a96
Caio Oliveira <caio.oliveira@redes.unb.br>
2022-03-09 08:36:27 -0300

    Main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import "./App.scss";
-import YugiohCardForm from "./components/YugiohCardForm";
-import YugiohCard from "./components/YugiohCard";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div>
-      <div className="alert alert-primary">Projeto Limpom com bootstrap</div>
-
-      <YugiohCardForm></YugiohCardForm>
-      <YugiohCard></YugiohCard>
+      <div className="alert alert-primary">Projeto Limpo com bootstrap</div>
+      <MainPage></MainPage>
     </div>
   );
 }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import YugiohCard from "../components/YugiohCard";
+import YugiohCardForm from "../components/YugiohCardForm";
+
+function MainPage() {
+  return (
+    <>
+      <h1>THIS IS THE MAIN PAGE</h1>
+      
+      <YugiohCardForm></YugiohCardForm>
+      <YugiohCard></YugiohCard>
+    </>
+  );
+}
+
+export default MainPage;
